98ed2eba19e523b2292233c7f4f1e75dd31b018a
gbPark <gbpark0524@gmail.com>
2024-07-31 03:44:44 +0900

    Added AppBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import './App.css';
+import ResponsiveAppBar from './component/AppBar';
 
 function App() {
     const [list] = useState(['남자 코트 추천', '강남우동맛집', '파이썬']);
@@ -9,32 +10,35 @@ function App() {
 
     return (
         <div className="App">
-            <div className="black-nav">
+            <ResponsiveAppBar />
+            <div style={{ marginTop: '64px' }}>
+                {
+                    list.map((item, i) => {
+                        return (
+                            <div className="list" key={i}>
+                                <h4 onClick={() => {
+                                    setModal(!modal);
+                                    setModalTitle(i);
+                                }}>{i}. {item} <span onClick={() => {
+                                    const clone = [...thumb];
+                                    clone[i]++;
+                                    setThumb(clone);
+                                }}>따봉</span></h4>{thumb[i]}
+                                <p>2월 17일 발행</p>
+                            </div>
+                        )
+                    })
+                }
+
+                {
+                    modal ? <Modal list={list} modalTitle={modalTitle}/> : null
+                }
+                {/*<Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                     추가할 라우트들 
+                </Routes>*/}
             </div>
-            <h4>블로그임</h4>
-
-            {
-                list.map((item, i) => {
-                    return (
-                        <div className="list" key={i}>
-                            <h4 onClick={() => {
-                                setModal(!modal);
-                                setModalTitle(i);
-                            }}>{i}. {item} <span onClick={() => {
-                                const clone = [...thumb];
-                                clone[i]++;
-                                setThumb(clone);
-                            }}>따봉</span></h4>{thumb[i]}
-                            <p>2월 17일 발행</p>
-                        </div>
-                    )
-                })
-            }
-
-            {
-                modal ? <Modal list={list} modalTitle={modalTitle}/> : null
-            }
-
         </div>
     );
 

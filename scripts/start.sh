150a712b6631b25070ce448423e176ccb544fb60
gbPark <gbpark0524@gmail.com>
2024-08-06 02:06:53 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo -E GBPARK_DB_URL=$GBPARK_DB_URL GBPARK_DB_USER=$GBPARK_DB_USER GBPARK_DB_PASS=$GBPARK_DB_PASS GBPARK_DB_DRIVER=$GBPARK_DB_DRIVER nohup java -jar -Dserver.port=80 /home/ec2-user/gbpark.jar > /home/ec2-user/app.log 2>&1 &
+sudo -E GBPARK_DB_URL="$GBPARK_DB_URL" GBPARK_DB_USER="$GBPARK_DB_USER" GBPARK_DB_PASS="$GBPARK_DB_PASS" GBPARK_DB_DRIVER="$GBPARK_DB_DRIVER" nohup java -jar -Dserver.port=80 /home/ec2-user/gbpark.jar > /home/ec2-user/app.log 2>&1 &

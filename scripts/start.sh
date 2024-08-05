0cf2317fc7f6bbd8e4bb116e27d1283af55a1ac1
gbPark <69848531+gbpark0524@users.noreply.github.com>
2024-08-06 02:32:37 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo -E nohup java -jar -Dserver.port=80 /home/ec2-user/gbpark.jar > /home/ec2-user/app.log 2>&1 &
+sudo bash -c 'source /home/ec2-user/bashrc && nohup java -jar -Dserver.port=80 /home/ec2-user/gbpark.jar > /home/ec2-user/app.log 2>&1 &'

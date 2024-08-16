9af0652260e08dee494108acac022661e7d8d4ac
gbPark <gbpark0524@gmail.com>
2024-08-17 02:07:20 +0900

    Added tooltip

--- frontend/src/component/portlet/GithubContributionPortlet.tsx
+++ frontend/src/component/portlet/GithubContributionPortlet.tsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import styles from '../../assets/styles/GithubContributionPortlet.module.scss'
+import Tooltip from "@mui/material/Tooltip";
 
 interface GithubResponse {
     date: string;
@@ -11,6 +12,7 @@ interface GithubResponse {
 interface Cell {
     commits: number;
     color: string;
+    date: string;
 }
 
 const GithubContributionPortlet: React.FC = () => {
@@ -23,13 +25,14 @@ const GithubContributionPortlet: React.FC = () => {
                 const recentData = response.data.slice(-16); // 최근 16일의 데이터
                 const cellData: Cell[] = recentData.map(day => ({
                     commits: day.commitCount,
-                    color: day.color
+                    color: day.color,
+                    date: day.date,
                 }));
                 setCells(cellData);
             } catch (error) {
                 console.error('Error fetching GitHub data:', error);
                 // 에러 시 빈 셀로 채움
-                setCells(Array(16).fill({ commits: 0, delay: 0, color: '#ebedf0' }));
+                setCells(Array(16).fill({commits: 0, delay: 0, color: '#ebedf0'}));
             }
         };
 
@@ -41,14 +44,27 @@ const GithubContributionPortlet: React.FC = () => {
             <div className={styles['plate-grid']}>
                 <div className={styles['area-grid']}>
                     {cells.map((cell, index) => (
-                        <div
-                            key={index}
-                            data-commits={cell.commits}
-                            className={styles['cell-' + index]}
-                            style={{
-                                backgroundColor: cell.color,
-                            }}
-                        />
+                        <Tooltip title={cell.commits.toString() + ' contributions on ' + cell.date } key={index} slotProps={{
+                            popper: {
+                                modifiers: [
+                                    {
+                                        name: 'offset',
+                                        options: {
+                                            offset: [0, -50],
+                                        },
+                                    },
+                                ],
+                            },
+                        }}>
+                            <div
+                                key={index}
+                                data-commits={cell.commits}
+                                className={styles['cell-' + index]}
+                                style={{
+                                    backgroundColor: cell.color,
+                                }}
+                            />
+                        </Tooltip>
                     ))}
                 </div>
             </div>

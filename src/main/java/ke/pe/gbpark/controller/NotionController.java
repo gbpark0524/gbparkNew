9bb38ebf54bb6f73e53f1eb9e5351d35a56c3a91
gbPark <gbpark0524@gmail.com>
2024-08-29 22:22:15 +0900

    Added Notion Controller

---
+++ src/main/java/ke/pe/gbpark/controller/NotionController.java
@@ -0,0 +1,26 @@
+package ke.pe.gbpark.controller;
+
+import ke.pe.gbpark.domain.NotionPageInfo;
+import ke.pe.gbpark.service.NotionService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/portlet/notion")
+public class NotionController {
+    private final NotionService notionService;
+
+    public NotionController(NotionService notionService) {
+        this.notionService = notionService;
+    }
+
+
+    @GetMapping("/list/{size}")
+    public List<NotionPageInfo> getNewNotionList(@PathVariable int size) {
+        return notionService.getNewNotionList(size);
+    }
+}
---
+++ src/test/java/ke/pe/gbpark/controller/NotionControllerTest.java
@@ -0,0 +1,36 @@
+package ke.pe.gbpark.controller;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+@Transactional
+class NotionControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    
+    @Test
+    void getNewNotionList() throws Exception {
+        // given
+        int size = 10;
+
+        // expected
+        mockMvc.perform(get("/portlet/notion/list/" + size)
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(size)))
+                .andDo(print());
+    }
+}

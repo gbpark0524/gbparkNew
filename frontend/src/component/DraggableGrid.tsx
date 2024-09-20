08d21f0d76f576c4773b3c2cc60b2827e1c75870
gbPark <gbpark0524@gmail.com>
2024-09-20 22:28:17 +0900

    Add images to DraggableGrid.tsx

--- frontend/src/component/DraggableGrid.tsx
+++ frontend/src/component/DraggableGrid.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import {Responsive, WidthProvider} from 'react-grid-layout';
-import {Paper} from '@mui/material';
-import GitHubPortlet from "@component/portlet/GitHubPortlet";
-import NotionPortlet from "@component/portlet/NotionPortlet";
-import GithubContributionPortlet from "@component/portlet/GithubContributionPortlet";
+import {Card, Paper} from '@mui/material';
+import NotionPortlet from '@component/portlet/NotionPortlet';
+import GithubContributionPortlet from '@component/portlet/GithubContributionPortlet';
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
+import devGbparkImage from '@image/dev-gbpark.webp';
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
 const DraggableGrid = () => {
     const layout = [
-        { i: 'a', x: 0, y: 0, w: 2, h: 3 },
+        { i: 'notion', x: 0, y: 0, w: 2, h: 3 },
+        { i: 'pic', x: 2, y: 0, w: 2, h: 3 },
         { i: 'gitHub', x: 0, y: 2, w: 7, h: 4 },
-        { i: 'c', x: 2, y: 0, w: 2, h: 3 }
     ];
 
     return (
         <ResponsiveGridLayout
-            className="layout"
+            className='layout'
             layouts={{ lg: layout }}
             breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
             cols={{ lg: 6, md: 4, sm: 2, xs: 1, xxs: 1 }}
@@ -28,14 +28,20 @@ const DraggableGrid = () => {
             isDraggable={false}
             style={{width: '100%'}}
         >
-            <Paper key="a" style={{ padding: 16 }}>
+            <Paper key='notion' style={{ padding: 16 }}>
                 <NotionPortlet/>
             </Paper>
-            <Paper key="gitHub" className={'flex-center'} style={{ padding: 16, overflow:'hidden'}}>
-                <GithubContributionPortlet/>
+            <Paper key='pic' style={{ padding: 16}}>
+                <Card className={'ff'}>
+                        <img
+                            src={devGbparkImage}
+                            loading='lazy'
+                            alt=''
+                        />
+                </Card>
             </Paper>
-            <Paper key="c" style={{ padding: 16}}>
-                <GitHubPortlet/>
+            <Paper key='gitHub' className={'flex-center'} style={{ padding: 16, overflow:'hidden'}}>
+                <GithubContributionPortlet/>
             </Paper>
         </ResponsiveGridLayout>
     );

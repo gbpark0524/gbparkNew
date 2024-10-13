2e4d4ab42af24356b318e9d144010c809a3a5d6f
gbPark <gbpark0524@gmail.com>
2024-10-14 02:43:28 +0900

    Fix start.sh about port

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,4 +2,4 @@
 set -a
 source /home/ec2-user/.env
 set +a
-sudo -E nohup java -jar -Dserver.port=80 /home/ec2-user/gbpark.jar > /home/ec2-user/app.log 2>&1 &
+sudo -E nohup java -jar /home/ec2-user/gbpark.jar > /home/ec2-user/app.log 2>&1 &

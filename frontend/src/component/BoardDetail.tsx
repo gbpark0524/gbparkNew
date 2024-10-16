0a90e08174326286a10f5ec37261a7ece706efda
gbPark <gbpark0524@gmail.com>
2024-10-16 23:07:30 +0900

    Add edit, delete button in boardDetail

--- frontend/src/component/BoardDetail.tsx
+++ frontend/src/component/BoardDetail.tsx
@@ -1,24 +1,38 @@
+import React from 'react';
 import styles from '@assets/styles/Board.module.scss';
+import {Button, Stack} from '@mui/material';
+import {Delete, Edit} from '@mui/icons-material';
 
 interface BoardDetailProps {
-    board : {
+    board: {
         title: string,
         writer: string,
         content: string,
     }
 }
-const BoardDetail = ({board} : BoardDetailProps) => {
+
+const BoardDetail = ({board}: BoardDetailProps) => {
     return (
-        <div className={styles['content']}>
-            <div className={styles['head']}>
-                <div className={styles['area-empty']}></div>
-                <div className={styles['title']}>{board.title}</div>
-                <div className={styles['writer']}>{board.writer}</div>
+        <Stack spacing={2} direction="column">
+            <div className={styles['content']}>
+                <div className={styles['head']}>
+                    <div className={styles['area-empty']}></div>
+                    <div className={styles['title']}>{board.title}</div>
+                    <div className={styles['writer']}>{board.writer}</div>
+                </div>
+                <div className={styles['cont-board']}>
+                    {board.content}
+                </div>
             </div>
-            <div className={styles['cont-board']}>
-                {board.content}
-            </div>
-        </div>
+            <Stack spacing={2} direction="row" justifyContent="flex-end">
+                <Button variant="outlined" startIcon={<Edit/>}>
+                    Edit
+                </Button>
+                <Button variant="contained" color="error" startIcon={<Delete/>}>
+                    Delete
+                </Button>
+            </Stack>
+        </Stack>
     );
 }
 
